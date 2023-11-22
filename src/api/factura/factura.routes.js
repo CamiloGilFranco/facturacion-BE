90c5754c7a3b8d6736abe4587bb3148195180362
Camilo Gil Franco <camilogilfranco@gmail.com>
2023-11-22 10:32:39 -0500

    correccion de bug de ruta de actulizacion de facturas

--- src/api/factura/factura.routes.js
+++ src/api/factura/factura.routes.js
@@ -7,7 +7,6 @@ const router = Router();
 router.post("/", facturaController.createInvoice);
 router.get("/", facturaController.findAllInvoices);
 router.get("/:id", facturaController.findOneInvoice);
-router.put("/:id", facturaController.updateInvoice);
 router.delete("/:id", facturaController.deleteInvoice);
 router.get("/report/main", facturaController.findReportData);
 
